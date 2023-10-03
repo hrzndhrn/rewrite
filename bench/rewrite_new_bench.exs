ea6202db7508b68c3244cc9e7709c14b6fe4761e
Marcus Kruse <esurk.sucram@gmail.com>
2023-10-03 09:26:17 +0200

    Update benchmark

--- bench/rewrite_new_bench.exs
+++ bench/rewrite_new_bench.exs
@@ -1,11 +1,13 @@
 defmodule RewriteNewBench do
   use BencheeDsl.Benchmark
 
+  config time: 20
+
   before_scenario do
     path = "tmp/bench"
     File.mkdir_p(path)
     File.cd!(path, fn ->
-      for x <- 1..100 do
+      for x <- 1..1_000 do
         File.write!("bench_#{x}.ex", ":bench" |> List.duplicate(x) |> Enum.join("\n") )
       end
     end)
